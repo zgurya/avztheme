cb29df1b5e67e6e59c3a16fb413575aeded8a701
azgur <azgur@DESKTOP-DIUBFBD>
2016-12-16 17:06:29 +0200

    Add files: header.php and footer.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
-<?php while ( have_posts() ) : the_post();?>
-	<h1><?php the_title();?></h1>
-	<?php the_content();?>
-<?php endwhile;?>
+<?php if ( have_posts() ) :?>
+	<main>
+		<?php while ( have_posts() ) : the_post();?>
+			<h1><?php the_title();?></h1>
+			<?php the_content();?>
+		<?php endwhile;?>
+	</main>
+<?php endif;?>
 <?php get_footer(); ?>
