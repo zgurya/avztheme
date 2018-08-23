203fafe8212ca638ff448fb3015ff451a3443d2a
zgurya <a.zgurya@gmail.com>
2018-08-24 00:03:58 +0300

    Add bootstrap

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
